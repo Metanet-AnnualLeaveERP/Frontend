e83e52ecee80085723010515676e5ef3614bc522
sojung lee <sojong1003@gmail.com>
2023-04-13 18:30:17 +0900

    REFACTOR : 구글 캘린더 api 공휴일 max time 변경

--- src/api/calendar-api.js
+++ src/api/calendar-api.js
@@ -14,7 +14,7 @@ export const getHolidays = () => {
   const apiKey = 'AIzaSyD7ZIm8LV39UVqb0RiOV0dXdUPE39xMi-M'
   const calendarId = 'ko.south_korea%23holiday%40group.v.calendar.google.com'
   const timeMin = '2023-01-01T00:00:00Z'
-  const timeMax = '2024-01-01T00:00:00Z'
+  const timeMax = '2026-01-01T00:00:00Z'
   const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&orderBy=startTime&singleEvents=true&timeMin=${timeMin}&timeMax=${timeMax}`
 
   return api({
