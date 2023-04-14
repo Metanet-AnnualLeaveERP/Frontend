f80fdfa1d4d075779404a06f7f328a1322d42b87
sojung lee <sojong1003@gmail.com>
2023-04-14 17:37:11 +0900

    FEAT : 파일 다운로드 , 업로드 구현 (다운로드는 테스트 버튼이라 후에 변경할 것)

--- src/api/file.js
+++ src/api/file.js
@@ -1,5 +1,6 @@
 import api from './api'
 
+/* 파일 업로드 */
 export const upload = (data) => {
   return api({
     url: '/files/upload',
@@ -8,5 +9,17 @@ export const upload = (data) => {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
+  })
+}
+
+/* 파일 다운로드 */
+export const download = (data) => {
+  console.log(data)
+  return api({
+    url: '/files/download',
+    // 파일 경로를 url에 보이지 않기 위해 post 전송
+    method: 'post',
+    data: data,
+    responseType: 'blob',
   })
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,10 @@
 /* 세부화된 api js들을 import하여 하나로 export
 실제 component 에서는 index.js를 import 하여 사용 */
+
+// 파일
+import { upload, download } from './file'
+export { upload, download }
+
+// 휴가 신청
+import { createRequest } from './vacation-request'
+export { createRequest }
