6fbe4254d6ece57b0b6e3a96130ad5d9085d3a37
sojung lee <sojong1003@gmail.com>
2023-04-16 21:42:27 +0900

    FEAT : 휴가 확인서 휴가 기간 포맷 변경

--- src/components/VacationRequestCertificate.vue
+++ src/components/VacationRequestCertificate.vue
@@ -10,8 +10,8 @@ export default {
 
   setup(props) {
     const alType = ref(false)
-    // const startDate = ref('')
-    // const endDate = ref('')
+    const startDate = ref('')
+    const endDate = ref('')
 
     onMounted(() => {
       //   console.log(props.item)
@@ -20,10 +20,27 @@ export default {
         console.log('연차이고 하루만 신청했을 경우 반차인지 아닌지 표시')
         alType.value = true
       }
+
+      // Date 객체로 변환
+      const start = new Date(props.item.startDate)
+      const end = new Date(props.item.endDate)
+
+      // 월, 일을 두 자리 숫자로 표현
+      const startMonth = String(start.getMonth() + 1).padStart(2, '0')
+      const startDay = String(start.getDate()).padStart(2, '0')
+
+      const endMonth = String(end.getMonth() + 1).padStart(2, '0')
+      const endDay = String(end.getDate()).padStart(2, '0')
+
+      // 변환된 문자열 생성
+      startDate.value = `${start.getFullYear()}년 ${startMonth}월 ${startDay}일`
+      endDate.value = `${start.getFullYear()}년 ${endMonth}월 ${endDay}일`
     })
 
     return {
       alType,
+      startDate,
+      endDate,
     }
   },
 }
@@ -61,7 +78,7 @@ export default {
               vertical-align: middle;
             "
             colspan="2"
-            class="dext_table_border_t dext_table_border_r dext_table_border_b dext_table_border_l"
+            class="dext_table_border_t dext_table_border_r dext_table_border_b dext_table_border_l bg-white dark:bg-foreground dark:text-white"
           >
             휴가 확인서
           </td>
@@ -69,18 +86,15 @@ export default {
         <tr>
           <td
             style="width: 300px; border: 0; padding: 0 !important"
-            class="dext_table_border_t dext_table_border_r dext_table_border_b dext_table_border_l"
+            class="dext_table_border_t dext_table_border_r dext_table_border_b dext_table_border_l bg-white dark:bg-foreground dark:text-white"
           >
             <table
               class="__se_tbl"
               style="
-                background: white;
                 margin: 0px;
                 border: 1px solid black;
                 border-image: none;
-                color: black;
                 font-family: malgun gothic, dotum, arial, tahoma;
-
                 border-collapse: collapse !important;
               "
             >
@@ -89,20 +103,21 @@ export default {
               <tbody>
                 <tr>
                   <td
+                    class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
                     style="
                       width: 100px;
                       height: 22px;
                       vertical-align: middle;
                       border: 1px solid black;
                       text-align: center;
-                      font-weight: bold;
-                      background: rgb(221, 221, 221);
                       padding: 3px !important;
                     "
                   >
                     기안자
                   </td>
+
                   <td
+                    class="bg-white dark:bg-foreground dark:text-white"
                     style="
                       width: 200px;
                       height: 22px;
@@ -151,6 +166,7 @@ export default {
                 </tr>
                 <tr>
                   <td
+                    class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
                     style="
                       width: 100px;
                       padding: 3px !important;
@@ -158,8 +174,6 @@ export default {
                       vertical-align: middle;
                       border: 1px solid black;
                       text-align: center;
-                      font-weight: bold;
-                      background: #ddd;
                     "
                   >
                     기안부서
@@ -213,6 +227,7 @@ export default {
                 </tr>
                 <tr>
                   <td
+                    class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
                     style="
                       width: 100px;
                       padding: 3px !important;
@@ -220,8 +235,6 @@ export default {
                       vertical-align: middle;
                       border: 1px solid black;
                       text-align: center;
-                      font-weight: bold;
-                      background: #ddd;
                     "
                   >
                     기안일
@@ -273,68 +286,7 @@ export default {
                     </span>
                   </td>
                 </tr>
-                <tr>
-                  <td
-                    style="
-                      width: 100px;
-                      padding: 3px !important;
-                      height: 22px;
-                      vertical-align: middle;
-                      border: 1px solid black;
-                      text-align: center;
-                      font-weight: bold;
-                      background: #ddd;
-                    "
-                  >
-                    문서번호
-                  </td>
-                  <td
-                    style="
-                      width: 200px;
-                      padding: 3px !important;
-                      height: 22px;
-                      vertical-align: middle;
-                      border: 1px solid black;
-                      text-align: left;
-                    "
-                  >
-                    <span
-                      unselectable="on"
-                      contenteditable="false"
-                      class="comp_wrap"
-                      data-cid="3"
-                      data-dsl="{{label:docNo}}"
-                      data-wrapper=""
-                      style=""
-                      data-value=""
-                      data-autotype=""
-                      ><span class="comp_item">문서번호</span
-                      ><span
-                        contenteditable="false"
-                        class="comp_active"
-                        style="display: none"
-                      >
-                        <span class="Active_dot1"></span
-                        ><span class="Active_dot2"></span>
-                        <span class="Active_dot3"></span
-                        ><span class="Active_dot4"></span>
-                      </span>
-                      <span
-                        contenteditable="false"
-                        class="comp_hover"
-                        data-content-protect-cover="true"
-                        data-origin="3"
-                      >
-                        <a
-                          contenteditable="false"
-                          class="ic_prototype ic_prototype_trash"
-                          data-content-protect-cover="true"
-                          data-component-delete-button="true"
-                        ></a>
-                      </span>
-                    </span>
-                  </td>
-                </tr>
+                <tr></tr>
               </tbody>
             </table>
           </td>
@@ -362,29 +314,24 @@ export default {
       <tbody>
         <tr>
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             style="
-              background: rgb(221, 221, 221);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: center;
-              color: rgb(0, 0, 0);
-
-              font-weight: bold;
               vertical-align: middle;
             "
           >
             휴가 종류
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
-              background: rgb(255, 255, 255);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: left;
-              color: rgb(0, 0, 0);
-
               font-weight: normal;
               vertical-align: middle;
             "
@@ -405,76 +352,66 @@ export default {
         </tr>
         <tr>
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             style="
-              background: rgb(221, 221, 221);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: center;
-              color: rgb(0, 0, 0);
-
-              font-weight: bold;
               vertical-align: middle;
             "
           >
             휴가 기간
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
               padding: 3px;
               border: 1px solid black;
               width: 700px;
               height: 22px;
               text-align: left;
-              color: rgb(0, 0, 0);
               vertical-align: middle;
-              background: rgb(255, 255, 255);
             "
             v-if="item.reqDays != 1"
           >
-            {{ item?.startDate }} - {{ item?.endDate }}
+            {{ startDate }} - {{ endDate }}
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
               padding: 3px;
               border: 1px solid black;
               width: 700px;
               height: 22px;
               text-align: left;
-              color: rgb(0, 0, 0);
               vertical-align: middle;
-              background: rgb(255, 255, 255);
             "
             v-else
           >
-            {{ item?.startDate }} - {{ item?.endDate }}
+            {{ item.startDate }} - {{ item?.endDate }}
           </td>
         </tr>
         <tr v-if="alType">
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             style="
-              background: rgb(221, 221, 221);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: center;
-              color: rgb(0, 0, 0);
-
-              font-weight: bold;
               vertical-align: middle;
             "
           >
             반차 여부
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
-              background: rgb(255, 255, 255);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: left;
-              color: rgb(0, 0, 0);
-
               font-weight: normal;
               vertical-align: middle;
             "
@@ -493,61 +430,53 @@ export default {
         </tr>
         <tr>
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             style="
-              background: rgb(221, 221, 221);
               padding: 5px;
               border: 1px solid black;
               height: 25px;
               text-align: center;
-              color: rgb(0, 0, 0);
-
-              font-weight: bold;
               vertical-align: middle;
             "
           >
             휴가 일수
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
               padding: 3px;
               border: 1px solid black;
               width: 700px;
               height: 22px;
               text-align: left;
-              color: rgb(0, 0, 0);
               vertical-align: middle;
-              background: rgb(255, 255, 255);
             "
           >
-            {{ item.reqDays }} 일
+            총 {{ item.reqDays }} 일
           </td>
         </tr>
         <tr>
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             style="
-              background: rgb(221, 221, 221);
               padding: 5px;
               border: 1px solid black;
               height: 80px;
               text-align: center;
-              color: rgb(0, 0, 0);
-              font-weight: bold;
               vertical-align: middle;
             "
           >
             휴가 사유
           </td>
           <td
+            class="bg-white dark:bg-foreground dark:text-white"
             style="
               padding: 3px;
               border: 1px solid black;
               width: 700px;
               height: 100px;
               text-align: left;
-              color: rgb(0, 0, 0);
-
               vertical-align: top;
-              background: rgb(255, 255, 255);
             "
           >
             {{ item.comments }}
@@ -555,6 +484,7 @@ export default {
         </tr>
         <tr>
           <td
+            class="bg-gray-300 font-bold dark:bg-gray-700 dark:text-white"
             colspan="2"
             style="
               width: 800px;
@@ -563,12 +493,11 @@ export default {
               vertical-align: middle;
               border: 1px solid black;
               text-align: left;
-              background: #ddd;
             "
           >
             1. 연차의 사용은 근로기준법에 따라 전년도에 발생한 개인별 잔여
-            연차에 한하여 사용함을 원칙으로 한다. 단, 최초 입사시에는 근로
-            기준법에 따라 발생 예정된 연차를 차용하여 월 1회 사용 할 수 있다.<br />
+            연차에 한하여 사용함을 원칙으로 한다. 단, 최초 입사 시에는 근로
+            기준법에 따라 발생 예정된 연차를 차용하여 월 1회 사용할 수 있다.<br />
             2. 경조사 휴가는 행사일을 증명할 수 있는 가족 관계 증명서 또는 등본,
             청첩장 등 제출<br />
             3. 공가(예비군/민방위)는 사전에 통지서를, 사후에 참석증을 반드시
