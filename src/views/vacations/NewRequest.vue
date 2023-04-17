89db152004a95414ac9438672bab0eecc77161ac
sojung lee <sojong1003@gmail.com>
2023-04-17 16:27:07 +0900

    FEAT : 돌아가기 버튼 추가

--- src/views/vacations/NewRequest.vue
+++ src/views/vacations/NewRequest.vue
@@ -184,12 +184,17 @@
                 {현재 잔여일수 - reqdays}일입니다.
               </p>
             </div>
-            <div id="btn-wrapper" class="flex justify-start">
+            <div id="btn-wrapper" class="flex justify-between mb-5">
               <BaseBtn
                 type="submit"
-                class="mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 신청하기
+              </BaseBtn>
+              <BaseBtn
+                class="text-white bg-light hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                돌아가기
               </BaseBtn>
             </div>
 
--- src/views/vacations/RequestDetail.vue
+++ src/views/vacations/RequestDetail.vue
@@ -84,7 +84,7 @@ const onClickBackBtn = () => {
         첨부파일 다운로드
       </BaseBtn>
       <BaseBtn
-        class="mr-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="mr-3 text-white bg-light hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         @click="onClickBackBtn"
       >
         돌아가기
