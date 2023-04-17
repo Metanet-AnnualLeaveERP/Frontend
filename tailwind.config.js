ba894b2223a592c53b955a1a642d632c2e5eeb08
sojung lee <sojong1003@gmail.com>
2023-04-17 16:23:50 +0900

    REFACTOR : bg-primary 색 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,30 +11,30 @@
  */
 
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                transparent: 'transparent',
-                current: 'currentColor',
-                primary: '#a855f7',
-                success: '#10b981',
-                danger: '#ef4444',
-                warning: '#f59e0b',
-                info: '#3b82f6',
-                light: '#6b7280',
-                dark: '#202020',
-                foreground: '#292929',
-            },
-            textColor: {
-                test: '#ef4444',
-            },
-            transitionDuration: {
-                240: '240ms',
-            },
-        },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: '#3b82f6',
+        success: '#10b981',
+        danger: '#ef4444',
+        warning: '#f59e0b',
+        info: '#3b82f6',
+        light: '#6b7280',
+        dark: '#202020',
+        foreground: '#292929',
+      },
+      textColor: {
+        test: '#ef4444',
+      },
+      transitionDuration: {
+        240: '240ms',
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms')],
 }
